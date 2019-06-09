e7b7173bc2d3f078ef14e4b2afc2a0463027a32d
Rob M <robbis@gmail.com>
2019-06-08 19:03:37 -0700

    removing og renaming v2

--- README.md
+++ README.md
@@ -5,7 +5,4 @@ Rock Paper Scissors Lizard Spock Ruby application for Arnold Clark Student Hack 
 ## Summary
 
 ### game.rb
-This program was created after ~4 hours of learning/coding experience in Ruby.
-
-### game_v2.rb
 This program was updated after a further 5 hours of learning/coding in Ruby.
--- game.rb
+++ game.rb
@@ -1,136 +1,117 @@
 # enable (true) or disable (false) tension building features
-buildTensionMode = true
-
+build_tension_mode = true
 
 # welcome and setup
-puts "Hi, welcome to RPSLSp"
+puts 'Hi, welcome to RPSLSp'
 
-numToText = %w{Rock Paper Scissors Lizard Spock}
-playerWins = 0
-computerWins = 0
+replay = %w[y n]
+num_to_text = %w[Rock Paper Scissors Lizard Spock]
+player_wins = 0
+computer_wins = 0
 
+# victory text lines
+victory_conditions = {
+  '32' => 'Scissors cuts Paper!',
+  '21' => 'Paper covers Rock!',
+  '14' => 'Rock crushes Lizard!',
+  '45' => 'Lizard poisons Spock!',
+  '53' => 'Spock smashes Scissors!',
+  '34' => 'Scissors decapitates Lizard!',
+  '42' => 'Lizard eats Paper!',
+  '25' => 'Paper disproves Spock!',
+  '51' => 'Spock vaporizes Rock!',
+  '13' => 'Rock crushes Scissors!'
+}
 
 # game start loop
 loop do
-
-computerThrow = rand(1..5)
-puts
-puts "Choose your weapon - Rock (1), Paper (2), Scissors (3), Lizard (4), Spock (5): "
-playerThrow = gets.to_i
-
-
-# check for valid player input
-until playerThrow.between?(1,5) do
+  computer_throw = rand(1..5)
   puts
-  puts "Something doesn't seem quite right, please check the choices and try again."
-  playerThrow = gets.to_i
-end
+  puts 'Choose your weapon - Rock (1), Paper (2), Scissors (3), Lizard (4), Spock (5): '
+  player_throw = gets.to_i
 
-
-# victory text lines
-def victoryConditions(result)
-  case result
-  when '32'
-    puts "Scissors cuts Paper!"
-  when '21'
-    puts "Paper covers Rock!"
-  when '14'
-    puts "Rock crushes Lizard!"
-  when '45'
-    puts "Lizard poisons Spock!"
-  when '53'
-    puts "Spock smashes Scissors!"
-  when '34'
-    puts "Scissors decapitates Lizard!"
-  when '42'
-    puts "Lizard eats Paper!"
-  when '25'
-    puts "Paper disproves Spock!"
-  when '51'
-    puts "Spock vaporizes Rock!"
-  when '13'
-    puts "Rock crushes Scissors!"
+  # check for valid player input
+  until player_throw.between?(1,5) do
+    puts
+    puts 'Something doesn\'t seem quite right, please check the choices and try again.'
+    player_throw = gets.to_i
   end
-end
 
-
-# build the tension
-counter = 3
-if buildTensionMode
-  loop do
-    system "clear" or system "cls"
-    puts "Throwing in"
-    puts counter
-    sleep (1)
-    counter -= 1
-    if counter == 0
-      break
+  # build the tension
+  counter = 3
+  if build_tension_mode
+    loop do
+      system 'clear'
+      puts 'Throwing in'
+      puts counter
+      sleep(1)
+      counter -= 1
+      break if counter.zero?
     end
   end
+
+  # show the throws
+  puts
+  print "You threw #{num_to_text[player_throw - 1]}, computer threw... "
+  sleep(1) if build_tension_mode
+  puts num_to_text[computer_throw - 1]
+  sleep(1) if build_tension_mode
+
+  # results of match
+  if player_throw == computer_throw
+    puts
+    puts "Both players threw #{num_to_text[player_throw - 1]}. It's a draw!"
+  elsif (player_throw == 3 && computer_throw == 2) ||
+        (player_throw == 2 && computer_throw == 1) ||
+        (player_throw == 1 && computer_throw == 4) ||
+        (player_throw == 4 && computer_throw == 5) ||
+        (player_throw == 5 && computer_throw == 3) ||
+        (player_throw == 3 && computer_throw == 4) ||
+        (player_throw == 4 && computer_throw == 2) ||
+        (player_throw == 2 && computer_throw == 5) ||
+        (player_throw == 5 && computer_throw == 1) ||
+        (player_throw == 1 && computer_throw == 3)
+    winner = player_throw.to_s + computer_throw.to_s
+    puts
+    puts victory_conditions.fetch(winner)
+    player_wins += 1
+    puts
+    sleep(1) if build_tension_mode
+    puts 'You win! :D'
+  else
+    winner = computer_throw.to_s + player_throw.to_s
+    puts
+    puts victory_conditions.fetch(winner)
+    computer_wins += 1
+    puts
+    sleep(1) if build_tension_mode
+    puts 'Computer wins :('
+  end
+
+  # repeat round // break
+  sleep(1) if build_tension_mode
+  puts
+  puts 'Another round? (y/n)'
+  play_again = gets.chomp.downcase
+
+  until replay.include?(play_again)
+    puts
+    puts 'hmm, did you want to play again? (y/n)'
+    play_again = gets.chomp.downcase
+  end
+
+  if play_again == 'y'
+    system 'clear' if build_tension_mode
+  end
+
+  break if play_again == 'n'
 end
-
-
-# show the throws
-puts
-print "You threw #{numToText[playerThrow-1]}, computer threw... "
-sleep (1) if buildTensionMode
-puts numToText[computerThrow-1]
-sleep (1) if buildTensionMode
-
-
-# results of match
-if playerThrow == computerThrow
-  puts
-  puts "Both players threw #{numToText[playerThrow-1]}. It's a draw!"
-elsif (playerThrow == 3 && computerThrow == 2) || (playerThrow == 2 && computerThrow == 1) || 
-	(playerThrow == 1 && computerThrow == 4) || (playerThrow == 4 && computerThrow == 5) || 
-	(playerThrow == 5 && computerThrow == 3) || (playerThrow == 3 && computerThrow == 4) || 
-	(playerThrow == 4 && computerThrow == 2) || (playerThrow == 2 && computerThrow == 5) || 
-	(playerThrow == 5 && computerThrow == 1) || (playerThrow == 1 && computerThrow == 3)
-  winner = playerThrow.to_s + computerThrow.to_s
-  puts
-  victoryConditions(winner)
-  playerWins += 1
-  puts
-  sleep (1) if buildTensionMode
-  puts "You win! :D"
-else
-  winner = computerThrow.to_s + playerThrow.to_s
-  puts
-  victoryConditions(winner)
-  computerWins += 1
-  puts
-  sleep (1) if buildTensionMode
-  puts "Computer wins :("
-end
-
-
-# repeat round // break
-sleep (1) if buildTensionMode
-puts
-puts "Another round? (y/n)"
-playAgain = gets.chomp.downcase
-
-until (playAgain == 'y' || playAgain == 'n') do
-  puts
-  puts "hmm, did you want to play again? (y/n)"
-  playAgain = gets.chomp.downcase
-end
-
-if playAgain == 'y'
-  system "clear" or system "cls" if buildTensionMode
-end
-
-break if playAgain == 'n'
-
-end
-
 
 # exit messages
 puts
-puts "Congratulations! You beat the computer #{playerWins} rounds to #{computerWins}" if playerWins > computerWins
-puts "The computer beat you #{computerWins} rounds to #{playerWins}. Better luck next time! " if computerWins > playerWins
-puts "WOW! A draw! Both you and the computer won #{playerWins} rounds" if playerWins == computerWins
+puts "Congratulations! You beat the computer #{player_wins} rounds to #{computer_wins}" if player_wins > computer_wins
+puts "The computer beat you #{computer_wins} rounds to #{player_wins}. Better luck next time!" if computer_wins > player_wins
+puts "WOW! A draw! Both you and the computer won #{player_wins} rounds" if player_wins == computer_wins
 puts
-puts "Thanks for playing! Press return to exit"
+puts 'Thanks for playing! Press return to exit'
 gets
